30f0353fd136b206f6aaba92f5fe36e4c475a0f6
panjyar <pranjiyarkehuli@gmail.com>
2025-03-16 20:29:43 +0530

    fix:Data is storing in DB

--- aictcscraper.py
+++ aictcscraper.py
@@ -3,6 +3,7 @@ import asyncio
 from urllib.parse import urljoin
 from crawl4ai import AsyncWebCrawler, CrawlerRunConfig, CacheMode
 from crawl4ai.extraction_strategy import JsonCssExtractionStrategy
+from mongodb_handler import MongoDBHandler
 
 async def extract_notices_and_events():
     urls = [
@@ -12,14 +13,17 @@ async def extract_notices_and_events():
                 "name": "CIT Notices and Events",
                 "baseSelector": "#myTabContent .tab-pane ul li , div.bxslider.bx-nav-top > div.border-1px.border-left-theme-color-2-6px",
                 "fields": [
+                    
                     {"name": "notice_title", "selector": "#tab1 a", "type": "text"},
                     {"name": "notice_url", "selector": "#tab1 a", "type": "attribute", "attribute": "href"},
+                    
                     {"name": "tender_title", "selector": "#tab2 a", "type": "text"},
                     {"name": "tender_url", "selector": "#tab2 a", "type": "attribute", "attribute": "href"},
+                    
                     {"name": "upcoming_Event_title", "selector": ".testimonial p a", "type": "text"},
                     {"name": "upcoming_Event_url", "selector": ".testimonial p a", "type": "attribute", "attribute": "href"},
-                    {"name": "event_date", "selector": ".event-date li.font-18", "type": "text"},
-                    {"name": "event_year", "selector": ".event-date li.font-14", "type": "text"}
+                    {"name": "upcoming_event_date", "selector": ".event-date li.font-18", "type": "text"},
+                    {"name": "upcoming_event_year", "selector": ".event-date li.font-14", "type": "text"}
                 ]
             }
         },
@@ -29,8 +33,10 @@ async def extract_notices_and_events():
                 "name": "NITA Notices and Upcoming Event",
                 "baseSelector": "div.event_box, div.notice-board ",
                 "fields": [
+                    
                     {"name": "notice_title", "selector": "a#ContentPlaceHolder1_Repeater_Announcement_hyprAnnmnt_0", "type": "text"},
                     {"name": "notice_url", "selector": "a#ContentPlaceHolder1_Repeater_Announcement_hyprAnnmnt_0", "type": "attribute", "attribute": "href"},
+                   
                     {"name": "upcoming_Event_title", "selector": "a#ContentPlaceHolder1_Repeater_Events_EvnthlALb1_0", "type": "text"},
                     {"name": "upcoming_Event_url", "selector": "a#ContentPlaceHolder1_Repeater_Events_EvnthlALb1_0", "type": "attribute", "attribute": "href"},
                     {"name": "upcoming_event_date", "selector": ".d-flex.align-items-center.mb-3  p.mb-0 ", "type": "text"},
@@ -43,8 +49,10 @@ async def extract_notices_and_events():
             "name": "IITB Announcements",
             "baseSelector": "div.view-content.row >div.views-row > div.views-field.views-field-nothing",
             "fields": [
+                
                 {"name": "news", "selector": ".field-content a", "type": "text"},
                 {"name": "news_url", "selector": ".field-content a", "type": "attribute", "attribute": "href"},
+               
                 # {"name": "newsss", "selector": ".news-card-title", "type": "text"},
                 # {"name": "newsss_url", "selector": ".news-card-more a", "type": "attribute", "attribute": "href"}
             ]
@@ -56,10 +64,13 @@ async def extract_notices_and_events():
            "name": "IITG Notices and Events",
                "baseSelector": "#myTabContent .tab-pane ul li , div.bxslider.bx-nav-top > div.border-1px.border-left-theme-color-2-6px",
                 "fields": [
+                   
                     {"name": "admission", "selector": "#tab1 a", "type": "text"},
                     {"name": "admission_url", "selector": "#tab1 a", "type": "attribute", "attribute": "href"},
+                    
                     {"name": "recruitment_title", "selector": "#tab2 a", "type": "text"},
                     {"name": "recruitment_url", "selector": "#tab2 a", "type": "attribute", "attribute": "href"},
+                    
                     {"name": "upcoming_Event_title", "selector": ".testimonial p a", "type": "text"},
                     {"name": "upcoming_Event_url", "selector": ".testimonial p a", "type": "attribute", "attribute": "href"},
                     {"name": "upcoming_event_date", "selector": ".event-date li.font-18", "type": "text"},
@@ -73,8 +84,10 @@ async def extract_notices_and_events():
         "name": "IIT Madras Announcements",
         "baseSelector": "div.row.display__flexbox > div.col-sm-4 , div.main-region",
         "fields": [
+           
             {"name": "news", "selector": "a h3", "type": "text"},
             {"name": "news_url", "selector": ".col-sm-4 a.block__element", "type": "attribute", "attribute": "href", "prefix": "https://www.iitm.ac.in"},
+           
             {"name": "research", "selector": ".col-sm-6.col-sm-6 h4.section__cardheading", "type": "text"},
             {"name": "research_url", "selector": ".col-sm-6.col-sm-6 a.block__element", "type": "attribute", "attribute": "href"}
         ] 
@@ -86,29 +99,32 @@ async def extract_notices_and_events():
         "name": "IIT Kharagpur Updates",
         "baseSelector": "div.upcoming-events div.item.bg-white.border.p-4 , div.owl-stage-outer  div.row.align-items-center",
         "fields": [
+          
             {"name" : "admission", "selector": ".col-lg-4 span a" , "type" : "text"},
             {"name" : "admission_url" , "selector" :".col-lg-8 p a" , "type" : "attribute" , "attribute" : "href"},
+           
             {"name": "upcoming_event_date","selector": "span.fz18", "type": "text" },
             {"name": "upcoming_event_title", "selector": "p.mb-0 span.w-75","type": "text"},
             {"name": "upcoming_event_url","selector": "p.mb-0 span.text-right a","type": "attribute","attribute": "href"}
         ]
     }
-}
-,
+    },
     {
     "url": "https://www.nitt.edu/",
     "schema": {
         "name": "NIT Trichy Updates",
         "baseSelector": ".wpb_column.col-md-4 ul.list-unstyled li , div.author ", ## make for upcomming event
         "fields": [
+               
                 {"name": "admission","selector": "a","type": "text"},
                 {"name": "admission_url","selector": "a","type": "attribute","attribute": "href"} ,
+                
                 {"name": "upcoming_event_title", "selector": "h3.upcoming-header a", "type": "text"},
                 {"name": "upcoming_event_url", "selector": "h3.upcoming-header a", "type": "attribute", "attribute": "href"},
                 {"name": "upcoming_event_date", "selector": "figure.date", "type": "text"}
         ]
     }
-},
+    },
 
     {
     "url": "https://vit.ac.in/",
@@ -116,20 +132,24 @@ async def extract_notices_and_events():
         "name": "VIT News and Events",
         "baseSelector": ".exad-nt-news ul li, div.elementor-swiper div.elementor-testimonial div.event_listing.post-96677",
         "fields": [
+           
             {"name": "admission","selector": " li a span","type": "text"},
             {"name": "admission_url","selector": "li a","type": "attribute","attribute": "href"},
+           
             {"name": "upcoming_event_title", "selector": "div.wpem-event-title h3", "type": "text"},
             {"name": "upcoming_event_url", "selector": "a.wpem-event-action-url", "type": "attribute", "attribute": "href"},
             {"name": "upcoming_event_date", "selector": "span.wpem-event-date-time-text", "type": "text"} 
         ]
     }
-}
+    }
     ]
     
     def process_url(base_url, extracted_url):
             if extracted_url and not extracted_url.startswith("http"):
                 return urljoin(base_url, extracted_url.strip())
             return extracted_url
+    
+    mongo_handler = MongoDBHandler()
     
 
     async with AsyncWebCrawler(verbose=True) as crawler:
@@ -144,25 +164,43 @@ async def extract_notices_and_events():
                 continue
 
             data = json.loads(result.extracted_content)
+
+            collections = {
+                "notices": [],
+                "tenders": [],
+                "upcoming_events": [],
+                "recruitments": [],
+                "admissions": [],
+                "news": [],
+                "research": []
+            }
+
             for entry in data:
-                if "url" in entry:
-                    entry["url"] = process_url(site["url"], entry["url"])
                 if "news_url" in entry:
                     entry["news_url"] = process_url(site["url"], entry["news_url"])
+                    collections["news"].append(entry)
                 if "upcoming_Event_url" in entry:
                     entry["upcoming_Event_url"] = process_url(site["url"], entry["upcoming_Event_url"])
+                    collections["upcoming_events"].append(entry)
                 if "recruitment_url" in entry:
                     entry["recruitment_url"] = process_url(site["url"], entry["recruitment_url"])
+                    collections["recruitments"].append(entry)
                 if "research_url" in entry:
                     entry["research_url"] = process_url(site["url"], entry["research_url"])
+                    collections["research"].append(entry)
                 if "admission_url" in entry:
                     entry["admission_url"] = process_url(site["url"] , entry["admission_url"])
+                    collections["admissions"].append(entry)
             if site["url"] == "https://www.nitt.edu/":
                      data = data[:10]
-            
-
+        
             print(f"Extracted {len(data)} notices and events from {site['url']}")
             print(json.dumps(data, indent=2) if data else "No data found")
 
-# Run the async function
+            for collection_name, records in collections.items():
+             if records:
+                mongo_handler.insert_data(collection_name , records)
+
+    mongo_handler.close_connection()
+
 asyncio.run(extract_notices_and_events())
