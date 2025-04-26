5b063532c980cb1f3eade2f5c14a022051f458cb
panjyar <panjiyarkehuli@gmail.com>
2025-04-26 18:31:01 +0530

    Add env variables

--- frontend/src/services/apiServices.js
+++ frontend/src/services/apiServices.js
@@ -1,7 +1,7 @@
 // src/services/apiService.js
 import axios from 'axios';
 
-const BASE_URL = 'https://collegebuzz-backend-lto9.onrender.com/api';
+const BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
 export const fetchActiveRecords = async (collection, limit = 50) => {
   try {
